d80ef51930b80bd963859fb49e1192beeca21160
Кирилл Коншин <konshin21@mail.ru>
2022-12-29 02:20:55 +0300

    исправил репозитории

--- кр/src/main/java/com/example/demo/repo/TransactionsRepository.java
+++ кр/src/main/java/com/example/demo/repo/TransactionsRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repo;
 import com.example.demo.Entity.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionsRepository extends JpaRepository<Transactions, Integer > {
 }
